684d56299125bd0f2ee27f48788513394bd5b7ec
dmontero-07 <64108906+dmontero-07@users.noreply.github.com>
2022-05-16 19:41:51 -0500

    Update CrudTest.java

--- src/test/java/com/example/UadyVirtual/CrudTest.java
+++ src/test/java/com/example/UadyVirtual/CrudTest.java
@@ -105,7 +105,7 @@ public class CrudTest {
 			driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
 			pause(3000);
 			driver.findElement(By.xpath("//i")).click();
-			driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr[1]/td[5]/button")).click();
+			driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button")).click();
 			driver.findElement(By.name("name")).click();
 			driver.findElement(By.name("name")).clear();
 			driver.findElement(By.name("name")).sendKeys("Vicente Sleme");
