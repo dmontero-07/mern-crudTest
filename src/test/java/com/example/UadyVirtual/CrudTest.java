1366958a17f044ca50c6196bb3ccaf4ff6eebc27
dmontero-07 <64108906+dmontero-07@users.noreply.github.com>
2022-05-16 19:29:13 -0500

    Update CrudTest.java

--- src/test/java/com/example/UadyVirtual/CrudTest.java
+++ src/test/java/com/example/UadyVirtual/CrudTest.java
@@ -101,6 +101,7 @@ public class CrudTest {
 			driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[2]")).click();
 			driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Male'])[1]/following::div[2]")).click();
 			driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
+			pause(3000);
 			driver.findElement(By.xpath("//i")).click();
 			driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr[1]/td[5]/button")).click();
 			driver.findElement(By.name("name")).click();
@@ -108,8 +109,8 @@ public class CrudTest {
 			driver.findElement(By.name("name")).sendKeys("Vicente Sleme");
 			driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
 			//Assert - Mensaje de éxito ADD
-		    pause(3000);
-		    assertThat(driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[4]/div/p")).getText(),is("Successfully updated!"));		    
+		    	pause(3000);
+		    	assertThat(driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[4]/div/p")).getText(),is("Successfully updated!"));		    
 			driver.findElement(By.xpath("//i")).click();
 		  }
 		  
